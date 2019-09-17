01f0ce1829697e29b01688a6174984827454d465
Mihai Macaneata <themacaneata@gmail.com>
2019-09-17 16:51:46 +0300

    fix imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
-const View = require('./components/theme/View.jsx')
+const View = require('./components/theme/View')
 
-exports.Structure = require('./src/components/manage/Edit.jsx')
+exports.Structure = require('./src/components/manage/Edit')
 
 exports.layoutViews = {
     mosaic_tiles_view: View,
