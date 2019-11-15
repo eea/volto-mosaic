b49c9a8432993201823e5f07f881ae2aff10aa92
Mihai Macaneata <themacaneata@gmail.com>
2019-11-15 14:18:24 +0200

    attempt to fix loadash import problem

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = function(api) {
   api.cache(true);
 
   const presets = ['@babel/preset-env', '@babel/preset-react'];
-  const plugins = [require('@babel/plugin-proposal-class-properties'), '@babel/plugin-proposal-optional-chaining'];
+  const plugins = [require('@babel/plugin-proposal-class-properties'), '@babel/plugin-proposal-optional-chaining', "transform-runtime", "syntax-dynamic-import"];
 
   return {
     presets,
