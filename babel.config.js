9fcc38d0b8b4f5e4d03c18fbd05a135e2d140d80
Mihai Macaneata <themacaneata@gmail.com>
2019-11-15 16:52:00 +0200

    fix broken exports

--- babel.config.js
+++ babel.config.js
@@ -3,9 +3,9 @@ module.exports = function(api) {
 
   const presets = ['@babel/preset-env', '@babel/preset-react'];
   const plugins = [require('@babel/plugin-proposal-class-properties'), '@babel/plugin-proposal-optional-chaining', "transform-runtime", "syntax-dynamic-import"];
-
   return {
     presets,
     plugins,
+    "sourceType": "unambiguous"
   };
 };
