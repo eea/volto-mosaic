0bcf1e1b065bdbb994a1a4fcf99114dd828771e6
Mihai Macaneata <themacaneata@gmail.com>
2019-11-15 14:00:50 +0200

    add react grid layout dependency

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = function(api) {
   api.cache(true);
 
   const presets = ['@babel/preset-env', '@babel/preset-react'];
-  const plugins = [require('@babel/plugin-proposal-class-properties')];
+  const plugins = [require('@babel/plugin-proposal-class-properties'), '@babel/plugin-proposal-optional-chaining'];
 
   return {
     presets,
