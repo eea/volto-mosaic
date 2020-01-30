cbd7523c175525b213cebe87444fd7b86b89eb82
Mihai Macaneata <mihai.macaneata@eaudeweb.ro>
2020-01-30 13:18:54 +0200

    higher width laptop preview

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const GET_MOSAIC_SETTINGS = 'GET_MOSAIC_SETTINGS';
 
-export const breakpoints = { lg: 1549, md: 1086, sm: 718, xs: 480, xxs: 0 };
+export const breakpoints = { lg: 1549, md: 1449, sm: 718, xs: 480, xxs: 0 };
 export const screenSizes = {
   lg: 'Unspecified (default)',
   md: 'Laptop',
